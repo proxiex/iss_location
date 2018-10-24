a36f9021a5d6eaf1c2a8212a736c579265c50928
proxiex <difrentecho@gmail.com>
2018-10-24 05:40:05 +0100

    setup application home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UserService } from './services/user.service'
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'iss-location';
+  public isLoggedin: boolean;
+  loggedin = false;
+
+  constructor(
+    private userService: UserService
+  ) {
+    this.isLoggedin = userService.isLoggedIn();
+  }
+
+  userIsLoggedIn(logged: boolean) {
+    this.isLoggedin = logged;
+    this.loggedin = logged;
+  }
+
+
 }
